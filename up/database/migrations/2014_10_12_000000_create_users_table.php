a7f8f15b1dd05ac5b62dcf2623e25b2497c3b9ba
Ribamar FS <ribafs@gmail.com>
2020-10-15 13:19:13 -0300

    Ajustes nas migrations

--- up/database/migrations/2014_10_12_000000_create_users_table.php
+++ up/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -13,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
